3805f3aa19c9183e49213287ae2daa4d2f4fa042
Maude Pod <186677756+MaudePod@users.noreply.github.com>
2024-11-04 00:54:09 -0500

    updates classnames

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-document.querySelector(".dice_container").addEventListener('click', function () {
+document.querySelector(".dice-container").addEventListener('click', function () {
     this.requestSubmit();
 });
-document.querySelector(".dice_container").addEventListener('submit', function (e) {
+document.querySelector(".dice-container").addEventListener('submit', function (e) {
     e.preventDefault();
     fetch('quotes.json').then((response) => {
         response.text().then((response) => {
             const quotes = JSON.parse(response);
             Math.floor(Math.random() * 10);
             const quoteIndex = Math.floor(Math.random() * quotes.length);
-            document.querySelector(".card_body_text").innerHTML = quotes[quoteIndex].quote.trim();
-            document.querySelector(".card_title").innerHTML = quotes[quoteIndex].id;
+            document.querySelector(".card-body-text").innerHTML = quotes[quoteIndex].quote.trim();
+            document.querySelector(".card-title").innerHTML = quotes[quoteIndex].id;
         })
     }
     )
